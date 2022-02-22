895c56ee2a4e5cff3685e9260e1ba301af0f8864
IndieGabo <indiegabodev@gmail.com>
2022-02-22 19:27:26 -0300

    Removing matches

--- Assets/Scripts/Game Modes/Simple Mode/Simple Mode States/SimpleModeStateEvaluateMatches.cs
+++ Assets/Scripts/Game Modes/Simple Mode/Simple Mode States/SimpleModeStateEvaluateMatches.cs
@@ -24,23 +24,6 @@ public class SimpleModeStateEvaluateMatches : SimpleModeState
 
         Debug.Log("Verificando matches...");
 
-        for (int x = 0; x < _simpleMode.core.board.xSize; x++)
-        {
-            for (int y = 0; y < _simpleMode.core.board.ySize; y++)
-            {
-                //Check adjascent tiles to detect a match
-                var tile = _simpleMode.core.board.GetTile(new Vector2(x, y));
-                var tile2 = _simpleMode.core.board.GetTile(new Vector2(x - 1, y));
-                var tile3 = _simpleMode.core.board.GetTile(new Vector2(x - 2, y));
-                if ((tile != null && tile2 != null && tile3 != null) && (tile.item.type == tile2.item.type && tile2.item.type == tile3.item.type))
-                {
-                    tile.item.transform.position = new Vector3(999f, 999f, -5f);
-                    tile2.item.transform.position = new Vector3(999f, 999f, -5f);
-                    tile3.item.transform.position = new Vector3(999f, 999f, -5f);
-                }
-            }
-        }
-
         _simpleMode.stateMachine.SetActiveState(_simpleMode.simpleModeStateInputCheck);
     }
 
